e33715b1bd84137f7d56a5b170b5af26be2e881f
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2009-05-03 21:45:23 +0900

    Data for both monoisotopic and molecular (average) amino acid mass
    Ignore-this: 4348dfdbd3f68a32895d42ec114bdf60
    
    darcs-hash:20090503124523-9241b-1a75a8322cdddda547a07afe6861f0b78c428c0c.gz

--- src/AminoAcid.hs
+++ src/AminoAcid.hs
@@ -1,5 +1,5 @@
 {-
- - Utilities for proteins sequences and their amino acids
+ - Utilities for amino acids
  -}
 
 module AminoAcid where
@@ -11,49 +11,73 @@ module AminoAcid where
 -- This corresponds to the mass of the residue, where the mass of the
 -- peptide/protein is the mass of the residue plus the mass of water.
 --
-getAAMass     :: Char -> Double
-getAAMass 'A' =   71.037114         -- Alanine              Alg     C3H5NO
-getAAMass 'R' =  156.101111         -- Arginine             Arg     C6H12N4O
-getAAMass 'N' =  114.042927         -- Asparagine           Asn     C4H6N2O2
-getAAMass 'D' =  115.026943         -- Aspartic acid        Asp     C4H5NO3
-getAAMass 'C' =  103.009185 + 57.0  -- Cysteine             Cys     C3H5NOS (plus alkylation??)
-getAAMass 'E' =  129.042593         -- Glutamic acid        Glu     C5H7NO3
-getAAMass 'Q' =  128.058578         -- Glutamine            Gln     C5H8N2O2
-getAAMass 'G' =   57.021464         -- Glycine              Gly     C2H3NO
-getAAMass 'H' =  137.058912         -- Histidine            His     C6H11NO
-getAAMass 'I' =  113.084064         -- Isoleucine           Ile     C6H11NO
-getAAMass 'L' =  113.084064         -- Leucine              Leu     C6H11NO
-getAAMass 'K' =  128.094963         -- Lysine               Lys     C6H12N2O
-getAAMass 'M' =  131.040485         -- Methionine           Met     C5H9NOS
-getAAMass 'F' =  147.068414         -- Phenylalanine        Phe     C9H9NO
-getAAMass 'P' =   97.052764         -- Proline              Pro     C5H7NO
-getAAMass 'O' =  114.07931          -- Pyrrolysine          Pyl     C12H21N3O3
-getAAMass 'U' =  150.04344          -- Selenocysteine       Sec     C3H5NOSe
-getAAMass 'S' =   87.032028         -- Serine               Ser     C3H5NO2
-getAAMass 'T' =  101.047679         -- Threonine            Thr     C4H7NO2
-getAAMass 'W' =  186.079313         -- Tryptophan           Trp     C11H10N2O
-getAAMass 'Y' =  163.06332          -- Tyrosine             Tyr     C9H9NO2
-getAAMass 'V' =   99.068414         -- Valine               Val     C5H9NO
+getAAMassMono     :: Char -> Float
+getAAMassMono 'A' =   71.037114         -- Alanine              Alg     C3H5NO
+getAAMassMono 'R' =  156.101111         -- Arginine             Arg     C6H12N4O
+getAAMassMono 'N' =  114.042927         -- Asparagine           Asn     C4H6N2O2
+getAAMassMono 'D' =  115.026943         -- Aspartic acid        Asp     C4H5NO3
+getAAMassMono 'C' =  103.009185         -- Cysteine             Cys     C3H5NOS
+getAAMassMono 'E' =  129.042593         -- Glutamic acid        Glu     C5H7NO3
+getAAMassMono 'Q' =  128.058578         -- Glutamine            Gln     C5H8N2O2
+getAAMassMono 'G' =   57.021464         -- Glycine              Gly     C2H3NO
+getAAMassMono 'H' =  137.058912         -- Histidine            His     C6H11NO
+getAAMassMono 'I' =  113.084064         -- Isoleucine           Ile     C6H11NO
+getAAMassMono 'L' =  113.084064         -- Leucine              Leu     C6H11NO
+getAAMassMono 'K' =  128.094963         -- Lysine               Lys     C6H12N2O
+getAAMassMono 'M' =  131.040485         -- Methionine           Met     C5H9NOS
+getAAMassMono 'F' =  147.068414         -- Phenylalanine        Phe     C9H9NO
+getAAMassMono 'P' =   97.052764         -- Proline              Pro     C5H7NO
+getAAMassMono 'O' =  114.07931          -- Pyrrolysine          Pyl     C12H21N3O3
+getAAMassMono 'U' =  150.04344          -- Selenocysteine       Sec     C3H5NOSe
+getAAMassMono 'S' =   87.032028         -- Serine               Ser     C3H5NO2
+getAAMassMono 'T' =  101.047679         -- Threonine            Thr     C4H7NO2
+getAAMassMono 'W' =  186.079313         -- Tryptophan           Trp     C11H10N2O
+getAAMassMono 'Y' =  163.06332          -- Tyrosine             Tyr     C9H9NO2
+getAAMassMono 'V' =   99.068414         -- Valine               Val     C5H9NO
 
 --
 -- Ambiguous amino acids
 --
-getAAMass 'B' =  114.53494          -- Aspargine            Asx     C4H8N2O3
-getAAMass 'J' =  113.16472          -- Leucine              Xle     C6H13NO2
-getAAMass 'Z' =  128.55059          -- Glutamine            Glx     C5H10N2O3
-getAAMass 'X' =  113.08406          -- Unknown              Xaa
+getAAMassMono 'B' =  114.53494          -- Aspargine            Asx     C4H8N2O3
+getAAMassMono 'J' =  113.16472          -- Leucine              Xle     C6H13NO2
+getAAMassMono 'Z' =  128.55059          -- Glutamine            Glx     C5H10N2O3
+getAAMassMono 'X' =  113.08406          -- Unknown              Xaa
 
-getAAMass  x  = error $ "Unknown amino acid abbreviation: " ++ [x]
+getAAMassMono  x  = error $ "Unknown amino acid abbreviation: " ++ [x]
 
 
-{-
--- Explicitly building an unboxed-array for the lookup-table leads to
--- complicated core, which isn't actually any faster...
 --
-getAAMass   :: Char -> Double
-getAAMass x =  amino ! (fromEnum x)
-    where
-        amino :: Array Int Double
-        amino = listArray (fromEnum 'A', fromEnum 'Z') 
-            [71.037114, 114.10272, 103.009185+57.0, 115.026943, 129.042593, 147.068414, 57.021464, 137.058912, 113.084064, 113.16472, 128.094963, 113.084064, 131.040485, 114.042927, 255.15829, 97.052764, 128.058578, 156.101111, 87.032028, 101.047679, 168.053, 99.068414, 186.079313, 0, 163.06332, 128.12472]
- -}
+-- Return the average (molecular) mass of an amino acid residue in atomic mass
+-- units, for the given short abbreviation.
+--
+getAAMassAvg     :: Char -> Float
+getAAMassAvg 'A' =   71.0788            -- Alanine              Alg
+getAAMassAvg 'R' =  156.1875            -- Arginine             Arg
+getAAMassAvg 'N' =  114.1038            -- Asparagine           Asn
+getAAMassAvg 'D' =  115.0886            -- Aspartic acid        Asp
+getAAMassAvg 'C' =  103.1388            -- Cysteine             Cys
+getAAMassAvg 'E' =  129.1155            -- Glutamic acid        Glu
+getAAMassAvg 'Q' =  128.1307            -- Glutamine            Gln
+getAAMassAvg 'G' =   57.0519            -- Glycine              Gly
+getAAMassAvg 'H' =  137.1411            -- Histidine            His
+getAAMassAvg 'I' =  113.1594            -- Isoleucine           Ile
+getAAMassAvg 'L' =  113.1594            -- Leucine              Leu
+getAAMassAvg 'K' =  128.1741            -- Lysine               Lys
+getAAMassAvg 'M' =  131.1926            -- Methionine           Met
+getAAMassAvg 'F' =  147.1766            -- Phenylalanine        Phe
+getAAMassAvg 'P' =   97.1167            -- Proline              Pro
+getAAMassAvg 'O' =  114.1472            -- Pyrrolysine          Pyl
+getAAMassAvg 'U' =  150.0388            -- Selenocysteine       Sec
+getAAMassAvg 'S' =   87.0782            -- Serine               Ser
+getAAMassAvg 'T' =  101.1051            -- Threonine            Thr
+getAAMassAvg 'W' =  186.2132            -- Tryptophan           Trp
+getAAMassAvg 'Y' =  163.1760            -- Tyrosine             Tyr
+getAAMassAvg 'V' =   99.1326            -- Valine               Val
+
+getAAMassAvg 'B' =  114.5962            -- Aspargine            Asx
+getAAMassAvg 'J' =  113.240178          -- Leucine              Xle
+getAAMassAvg 'Z' =  128.6231            -- Glutamine            Glx
+getAAMassAvg 'X' =  113.1594            -- Unknown              Xaa
+
+getAAMassAvg  x  = error $ "Unknown amino acid abbreviation: " ++ [x]
+
