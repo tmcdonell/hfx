9c84480d264b3ffab70f0922fa51307bbb1a484f
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2009-05-07 08:44:57 +0900

    Intensity spectrum from peptide AA sequence
    Ignore-this: 953266fed4cc3851a1dc12e47108190b
    
    darcs-hash:20090506234457-9241b-49f1358a36daedb021e096b36ff2a8b9a073fb1c.gz

---
+++ src/IonSeries.hs
@@ -0,0 +1,74 @@
+{-
+ - Functions to work with a series of ions, and generate theoretical spectra
+ - suitable for matching against experimental results with the sequest
+ - cross-correlation algorithm.
+ -}
+
+module IonSeries where
+
+import Config
+import Protein
+import Utils
+
+
+--------------------------------------------------------------------------------
+-- Data structures
+--------------------------------------------------------------------------------
+
+--
+-- The mz/intensity spectrum array for the theoretical spectrum. Keep this as a
+-- sparse array, as there are so few elements compared to the experimental data.
+--
+type XCorrSpecThry = [(Float, Float)]
+
+
+--------------------------------------------------------------------------------
+-- Theoretical Spectrum
+--------------------------------------------------------------------------------
+
+--
+-- Generate the theoretical spectral representation of a peptide from its
+-- character code sequence.
+--
+-- XXX: Apparently there are different types of ions other than the B- Y- and
+-- A-series we consider. Also, assumes a charge state of one.
+--
+buildThrySpecXCorr :: ConfigParams -> Peptide -> XCorrSpecThry
+buildThrySpecXCorr cp peptide =
+    concatMap (addIonsAB 1.0) b_ions ++ concatMap (addIonsY 1.0) y_ions
+    where
+        b_ions = init $ ladder peptide
+        y_ions = map (\x -> residual peptide - x) b_ions
+
+
+--
+-- Convert mass to mass/charge ratio
+--
+ionMZ :: Float -> Float -> Float
+ionMZ m c = (m + massH*c) / c
+
+
+--
+-- Add a spectral peak for each fragment location, as well as the peaks
+-- corresponding to neutral losses of H2O and NH3, for the given charge / mass
+-- location
+--
+addIonsAB, addIonsY :: Float -> Float -> [(Float, Float)]
+addIonsAB charge mass = addIonsA : addIonsB
+    where
+        addIonsA = let m = ionMZ (mass - massCO) charge in (m, 10)
+        addIonsB = let m = ionMZ mass charge in
+          [
+            (m,50), (m+1,25), (m-1,25),
+            (m - massH2O/charge, 10),
+            (m - massNH3/charge, 10)
+          ]
+
+addIonsY charge mass = addIonY
+    where
+        addIonY = let m = ionMZ (mass + massH2O) charge in
+          [
+            (m,50), (m+1,25), (m-1,25),
+            (m - massNH3/charge, 10)
+          ]
+
