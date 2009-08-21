dd53b7243ddf728b050037a67efaf48374febbd5
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2009-08-21 15:29:51 +0900

    c utilities
    Ignore-this: 74b1bac6bcc341260d3d6d918752f190
    
    darcs-hash:20090821062951-9241b-76d87b2d262d6afd3230c5efd66a474b4e06bd69.gz

---
+++ cbits/utils.c
@@ -0,0 +1,22 @@
+/*
+ * Module    : Utils
+ * Copyright : (c) 2009 Trevor L. McDonell
+ * License   : BSD
+ */
+
+
+/*
+ * Compute the next highest power of two
+ */
+unsigned int
+ceilPow2(unsigned int x)
+{
+    --x;
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+    return ++x;
+}
+
---
+++ cbits/utils.h
@@ -0,0 +1,21 @@
+/*
+ * Module    : Utils
+ * Copyright : (c) 2009 Trevor L. McDonell
+ * License   : BSD
+ */
+
+
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned int ceilPow2(unsigned int x);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
