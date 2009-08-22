eeb90fb066294554f5f39c923d4158df7e84166c
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2009-08-22 16:07:09 +0900

    new build system
    Ignore-this: 13765ff72442af16770b2a48831a3ca2
    
    The old build system relied the makefile from the SDK, but this need not be
    installed to compile and run CUDA programs. So, this has been imported directly,
    and then extended with our rules for compiling Haskell programs.
    
    Additionally, we now compile the kernels to a static library, and then just have
    ghc link against that. I can't remember anymore why exactly I thought that would
    be a good idea.
    
    darcs-hash:20090822070709-9241b-6bc30af9881411d34c3df6a0c1097299f8194887.gz

--- common.mk
+++ common.mk
@@ -1,61 +1,284 @@
-#
-# Common bridge between Haskell and CUDA build systems
-# 
-# For the variables set below, it was required to edit the common.mk file from
-# the SDK to be optional defines (?=).
-#
-
 # ------------------------------------------------------------------------------
-# Common CUDA build system
+# Common Haskell/CUDA build system
 # ------------------------------------------------------------------------------
 
-CUDA_SDK_PATH	?= /Developer/GPU\ Computing/C
+.SUFFIXES : .cu .cu_dbg.o .c_dbg.o .cpp_dbg.o .cu_rel.o .c_rel.o .cpp_rel.o .cubin .ptx
 
-SRCDIR		?= src/
+# Add new SM Versions here as devices with new Compute Capability are released
+SM_VERSIONS	:= sm_10 sm_11 sm_12 sm_13
+
+# detect OS
+OSUPPER		:= $(shell uname -s 2>/dev/null | tr [:lower:] [:upper:])
+OSLOWER		:= $(shell uname -s 2>/dev/null | tr [:upper:] [:lower:])
+DARWIN		:= $(strip $(findstring DARWIN, $(OSUPPER)))
+
+# detect if 32 bit or 64 bit system
+HP_64		:= $(strip $(shell uname -m | grep 64))
+
+# Basic directory setup
+CUDA_INSTALL_PATH ?= /usr/local/cuda
+CUDA_SDK_PATH	  ?= /Developer/GPU\ Computing/C
+
+SRCDIR		?= src
 DISTROOT	?= dist
-BINDIR		 = $(DISTROOT)/bin
-ROOTOBJDIR	 = $(DISTROOT)/obj
-LIBDIR		 = $(CUDA_SDK_PATH)/lib
-COMMONDIR	 = $(CUDA_SDK_PATH)/common
+BINDIR		:= $(DISTROOT)/bin
+LIBDIR		:= $(DISTROOT)/lib
+ROOTOBJDIR	:= $(DISTROOT)/obj
 
-GHC_FLAGS	 = --make -Wall -lstdc++ \
-		   -i$(SRCDIR) -i$(ROOTOBJDIR)/$(BINSUBDIR) \
-		   -odir $(ROOTOBJDIR)/$(BINSUBDIR) -hidir $(ROOTOBJDIR)/$(BINSUBDIR)
+# Compilers
+NVCC		:= $(CUDA_INSTALL_PATH)/bin/nvcc
+GHC		:= ghc
+C2HS		:= c2hs
+HSC2HS		:= hsc2hs
+CXX		:= g++
+CC		:= gcc
+LINK		:= g++ -fPIC
 
-HSFILES		+= $(HSMAIN)
+# Includes
+INCLUDES	+= -I. -I$(CUDA_INSTALL_PATH)/include
 
-# small hack: dependencies of the target, but not passed to object linker
-#
-CUBINS		+= $(patsubst %.chs,$(OBJDIR)/%.hs,$(notdir $(filter %.chs,$(HSFILES))))
-CUBINS		+= $(patsubst %.hsc,$(OBJDIR)/%.hs,$(notdir $(filter %.hsc,$(HSFILES))))
+# architecture flag for cubin build
+CUBIN_ARCH_FLAG :=
 
+# Warning flags
+CXXWARN_FLAGS := \
+	-W -Wall \
+	-Wimplicit \
+	-Wswitch \
+	-Wformat \
+	-Wchar-subscripts \
+	-Wparentheses \
+	-Wmultichar \
+	-Wtrigraphs \
+	-Wpointer-arith \
+	-Wcast-align \
+	-Wreturn-type \
+	-Wno-unused-function
+
+CWARN_FLAGS := $(CXXWARN_FLAGS) \
+	-Wstrict-prototypes \
+	-Wmissing-prototypes \
+	-Wmissing-declarations \
+	-Wnested-externs \
+	-Wmain
+
+GHCWARN_FLAGS := \
+	-Wall
+
+# Compiler-specific flags
+NVCCFLAGS	:=
+GHCFLAGS	 = -i$(SRCDIR) -i$(OBJDIR) -odir $(OBJDIR) -hidir $(OBJDIR) --make
+CXXFLAGS	:= $(CXXWARN_FLAGS)
+CFLAGS		:= $(CWARN_FLAGS)
+
+# Common flags
+COMMONFLAGS	+= $(INCLUDES) -DUNIX
+
+# Debug/release configuration
 ifeq ($(dbg),1)
-    GHC_FLAGS	+= -prof -auto-all -caf-all -fhpc
+    COMMONFLAGS += -g
+    NVCCFLAGS   += -D_DEBUG
+    CXXFLAGS    += -D_DEBUG
+    CFLAGS      += -D_DEBUG
+    BINSUBDIR   := debug
+    LIBSUFFIX   := D
 else
-    GHC_FLAGS	+= -O3
+    COMMONFLAGS += -O2
+    BINSUBDIR   := release
+    LIBSUFFIX   :=
+    NVCCFLAGS   += --compiler-options -fno-strict-aliasing
+    CXXFLAGS    += -fno-strict-aliasing
+    CFLAGS      += -fno-strict-aliasing
 endif
 
-ifeq ($(suffix $(HSMAIN)),.hs)
-    LINK	 = ghc $(GHC_FLAGS) $(SRCDIR)$(HSMAIN)
+# architecture flag for cubin build
+CUBIN_ARCH_FLAG :=
+
+# Libraries
+LIB		:= -L$(LIBDIR) $(addprefix -l,$(EXTRALIBS))
+ifeq ($(HP_64),)
+   LIB       	+= -L$(CUDA_INSTALL_PATH)/lib
 else
-    LINK	 = ghc $(GHC_FLAGS) $(OBJDIR)/$(addsuffix .hs,$(basename $(HSMAIN)))
+   LIB       	+= -L$(CUDA_INSTALL_PATH)/lib64
 endif
+
+# If dynamically linking to CUDA and CUDART, we exclude the libraries from the LIB
+ifeq ($(USECUDADYNLIB),1)
+    LIB 	+= -ldl -rdynamic
+else
+    # static linking, we will statically link against CUDA and CUDART
+    ifeq ($(USEDRVAPI),1)
+        LIB 	+= -lcuda
+    else
+        LIB 	+= -lcudart
+    endif
+endif
+
+# Device emulation configuration
+ifeq ($(emu),1)
+    NVCCFLAGS   += -deviceemu
+    CUDACCFLAGS +=
+    BINSUBDIR   := emu$(BINSUBDIR)
+    LIBSUFFIX   := $(LIBSUFFIX)_emu
+    # consistency, makes developing easier
+    CXXFLAGS	+= -D__DEVICE_EMULATION__
+    CFLAGS	+= -D__DEVICE_EMULATION__
+endif
+
+# Library/executable configuration
+ifneq ($(STATIC_LIB),)
+    TARGETDIR	:= $(LIBDIR)
+    TARGET  	:= $(subst .a,$(LIBSUFFIX).a,$(LIBDIR)/$(STATIC_LIB))
+    LINKLINE 	 = ar rucv $(TARGET) $(OBJS)
+else
+    TARGETDIR	:= $(BINDIR)/$(BINSUBDIR)
+    TARGET   	:= $(TARGETDIR)/$(EXECUTABLE)
+    ifneq ($(HSMAIN),)
+        LINKLINE =  $(GHC) -o $(TARGET) $(LIB) $(GHCFLAGS) $(HSMAIN)
+    else
+        LINKLINE = $(LINK) -o $(TARGET) $(OBJS) $(LIB)
+    endif
+endif
+
+# check if verbose
+ifeq ($(verbose),1)
+    VERBOSE 	:=
+else
+    VERBOSE 	:= @
+endif
+
+
+# ------------------------------------------------------------------------------
+# Check for input flags and set compiler flags appropriately
+# ------------------------------------------------------------------------------
+ifeq ($(fastmath),1)
+    NVCCFLAGS 	+= -use_fast_math
+endif
+
+ifeq ($(keep),1)
+    NVCCFLAGS 	    += -keep
+    NVCC_KEEP_CLEAN := *.i* *.cubin *.cu.c *.cudafe* *.fatbin.c *.ptx
+endif
+
+ifdef maxregisters
+    NVCCFLAGS 	+= -maxrregcount $(maxregisters)
+endif
+
+# Add cudacc flags
+NVCCFLAGS += $(CUDACCFLAGS)
+
+# Add common flags
+NVCCFLAGS += $(COMMONFLAGS)
+CXXFLAGS  += $(COMMONFLAGS)
+CFLAGS    += $(COMMONFLAGS)
+
+ifeq ($(nvcc_warn_verbose),1)
+    NVCCFLAGS += $(addprefix --compiler-options ,$(CXXWARN_FLAGS))
+    NVCCFLAGS += --compiler-options -fno-strict-aliasing
+endif
+
+
+# ------------------------------------------------------------------------------
+# Set up object files
+# ------------------------------------------------------------------------------
+OBJDIR 	:= $(ROOTOBJDIR)/$(BINSUBDIR)
+OBJS 	+= $(patsubst %.cpp,$(OBJDIR)/%.cpp.o,$(notdir $(CCFILES)))
+OBJS 	+= $(patsubst %.c,$(OBJDIR)/%.c.o,$(notdir $(CFILES)))
+OBJS 	+= $(patsubst %.cu,$(OBJDIR)/%.cu.o,$(notdir $(CUFILES)))
+
+# ------------------------------------------------------------------------------
+# Set up cubin output files
+# ------------------------------------------------------------------------------
+CUBINDIR := $(SRCDIR)/data
+CUBINS   += $(patsubst %.cu,$(CUBINDIR)/%.cubin,$(notdir $(CUBINFILES)))
+
+# ------------------------------------------------------------------------------
+# Set up PTX output files
+# ------------------------------------------------------------------------------
+PTXDIR 	:= $(SRCDIR)/data
+PTXBINS += $(patsubst %.cu,$(PTXDIR)/%.ptx,$(notdir $(PTXFILES)))
+
 
 # ------------------------------------------------------------------------------
 # Rules
 # ------------------------------------------------------------------------------
-include $(COMMONDIR)/common.mk
+%.subdir :
+	$(VERBOSE)$(MAKE) -C $* $(MAKECMDGOALS)
 
-$(OBJDIR)/%.hs : $(SRCDIR)%.chs
-	$(VERBOSE)c2hs --include=$(OBJDIR) $(addprefix --cppopts=,$(INCLUDES)) --output-dir=$(OBJDIR) --output=$(notdir $@) $<
+$(OBJDIR)/%.c.o : $(SRCDIR)/%.c $(C_DEPS)
+	$(VERBOSE)$(CC) $(CFLAGS) -o $@ -c $<
 
-$(OBJDIR)/%.hs : $(SRCDIR)%.hsc
-	$(VERBOSE)hsc2hs $(INCLUDES) -o $@ $<
+$(OBJDIR)/%.cpp.o : $(SRCDIR)/%.cpp $(C_DEPS)
+	$(VERBOSE)$(CXX) $(CXXFLAGS) -o $@ -c $<
 
-$(SRCDIR)C2HS.hs :
-	$(VERBOSE)c2hs --output-dir=$(SRCDIR) --copy-library
+$(OBJDIR)/%.cu.o : $(SRCDIR)/%.cu $(CU_DEPS)
+	$(VERBOSE)$(NVCC) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -c $<
 
-spotless : clean
+$(OBJDIR)/%.hs : $(SRCDIR)/%.chs
+	$(VERBOSE)$(C2HS) --include=$(OBJDIR) $(addprefix --cppopts=,$(INCLUDES)) --output-dir=$(OBJDIR) --output=$(notdir $@) $<
+
+$(OBJDIR)/%.hs : $(SRCDIR)/%.hsc
+	$(VERBOSE)$(HSC2HS) $(INCLUDES) -o $@ $<
+
+$(CUBINDIR)/%.cubin : $(SRCDIR)/%.cu cubindirectory
+	$(VERBOSE)$(NVCC) $(CUBIN_ARCH_FLAG) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -cubin $<
+
+$(PTXDIR)/%.ptx : $(SRCDIR)/%.cu ptxdirectory
+	$(VERBOSE)$(NVCC) $(CUBIN_ARCH_FLAG) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -ptx $<
+
+#
+# The following definition is a template that gets instantiated for each SM
+# version (sm_10, sm_13, etc.) stored in SMVERSIONS.  It does 2 things:
+# 1. It adds to OBJS a .cu_sm_XX.o for each .cu file it finds in CUFILES_sm_XX.
+# 2. It generates a rule for building .cu_sm_XX.o files from the corresponding
+#    .cu file.
+#
+# The intended use for this is to allow Makefiles that use common.mk to compile
+# files to different Compute Capability targets (aka SM arch version).  To do
+# so, in the Makefile, list files for each SM arch separately, like so:
+#
+# CUFILES_sm_10 := mycudakernel_sm10.cu app.cu
+# CUFILES_sm_12 := anothercudakernel_sm12.cu
+#
+define SMVERSION_template
+OBJS += $(patsubst %.cu,$(OBJDIR)/%.cu_$(1).o,$(notdir $(CUFILES_$(1))))
+$(OBJDIR)/%.cu_$(1).o : $(SRCDIR)/%.cu $(CU_DEPS)
+	$(VERBOSE)$(NVCC) -o $$@ -c $$< $(NVCCFLAGS) -arch $(1)
+endef
+
+# This line invokes the above template for each arch version stored in
+# SM_VERSIONS.  The call funtion invokes the template, and the eval
+# function interprets it as make commands.
+$(foreach smver,$(SM_VERSIONS),$(eval $(call SMVERSION_template,$(smver))))
+
+$(TARGET): makedirectories $(OBJS) $(CUBINS) $(PTXBINS) Makefile $(addsuffix .subdir,$(SUBDIRS))
+	$(VERBOSE)$(LINKLINE)
+
+cubindirectory:
+	$(VERBOSE)mkdir -p $(CUBINDIR)
+
+ptxdirectory:
+	$(VERBOSE)mkdir -p $(PTXDIR)
+
+makedirectories:
+	$(VERBOSE)mkdir -p $(LIBDIR)
+	$(VERBOSE)mkdir -p $(OBJDIR)
+	$(VERBOSE)mkdir -p $(TARGETDIR)
+
+
+tidy : $(addsuffix .subdir,$(SUBDIRS))
+	$(VERBOSE)find . | egrep "#"  | xargs rm -f
+	$(VERBOSE)find . | egrep "\~" | xargs rm -f
+
+clean : tidy
+	$(VERBOSE)rm -f $(OBJS)
+	$(VERBOSE)rm -f $(CUBINS)
+	$(VERBOSE)rm -f $(PTXBINS)
+	$(VERBOSE)rm -f $(TARGET)
+	$(VERBOSE)rm -f $(NVCC_KEEP_CLEAN)
+
+clobber : clean
+	$(VERBOSE)rm -rf $(ROOTOBJDIR)
+
+spotless:
 	$(VERBOSE)rm -rf $(DISTROOT)
-	$(VERBOSE)rm -f  $(patsubst %.cu,%.linkinfo,$(notdir $(CUFILES)))
-
