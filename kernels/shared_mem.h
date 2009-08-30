3334300de11afae66710ba69a46db70b55404c43
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2009-08-30 21:28:01 +0900

    simplify file somewhat
    Ignore-this: eecc7ef59a422a748fec4c44c3344fa5
    
    darcs-hash:20090830122801-9241b-c9a4876629b6b03f5555000eac90c66338e977f0.gz

--- kernels/shared_mem.h
+++ kernels/shared_mem.h
@@ -52,8 +52,8 @@
  * </pre>
  */
 
-#ifndef _SHAREDMEM_H_
-#define _SHAREDMEM_H_
+#ifndef __SHARED_MEM_H__
+#define __SHARED_MEM_H__
 
 
 /** @brief Wrapper class for templatized dynamic shared memory arrays.
@@ -84,80 +84,29 @@ struct SharedMemory
 // int, uint, char, uchar, short, ushort, long, ulong, bool, float, and double
 // One could also specialize it for user-defined types.
 
-template <>
-struct SharedMemory <int>
-{
-    __device__ int* getPointer() { extern __shared__ int s_int[]; return s_int; }
-};
+#define SPEC_SHAREDMEM(T, name)                                                         \
+    template <> struct SharedMemory <T>                                                 \
+    {                                                                                   \
+        __device__ T* getPointer() { extern __shared__ T s_##name[]; return s_##name; } \
+    }
 
-template <>
-struct SharedMemory <unsigned int>
-{
-    __device__ unsigned int* getPointer() { extern __shared__ unsigned int s_uint[]; return s_uint; }
-};
+SPEC_SHAREDMEM(int,    int);
+SPEC_SHAREDMEM(char,   char);
+SPEC_SHAREDMEM(long,   long);
+SPEC_SHAREDMEM(short,  short);
+SPEC_SHAREDMEM(bool,   bool);
+SPEC_SHAREDMEM(float,  float);
+SPEC_SHAREDMEM(double, double);
 
-template <>
-struct SharedMemory <char>
-{
-    __device__ char* getPointer() { extern __shared__ char s_char[]; return s_char; }
-};
+SPEC_SHAREDMEM(unsigned int,   uint);
+SPEC_SHAREDMEM(unsigned char,  uchar);
+SPEC_SHAREDMEM(unsigned long,  ulong);
+SPEC_SHAREDMEM(unsigned short, ushort);
 
-template <>
-struct SharedMemory <unsigned char>
-{
-    __device__ unsigned char* getPointer() { extern __shared__ unsigned char s_uchar[]; return s_uchar; }
-};
+SPEC_SHAREDMEM(uchar4, uchar4);
 
-template <>
-struct SharedMemory <short>
-{
-    __device__ short* getPointer() { extern __shared__ short s_short[]; return s_short; }
-};
-
-template <>
-struct SharedMemory <unsigned short>
-{
-    __device__ unsigned short* getPointer() { extern __shared__ unsigned short s_ushort[]; return s_ushort; }
-};
-
-template <>
-struct SharedMemory <long>
-{
-    __device__ long* getPointer() { extern __shared__ long s_long[]; return s_long; }
-};
-
-template <>
-struct SharedMemory <unsigned long>
-{
-    __device__ unsigned long* getPointer() { extern __shared__ unsigned long s_ulong[]; return s_ulong; }
-};
-
-template <>
-struct SharedMemory <bool>
-{
-    __device__ bool* getPointer() { extern __shared__ bool s_bool[]; return s_bool; }
-};
-
-template <>
-struct SharedMemory <float>
-{
-    __device__ float* getPointer() { extern __shared__ float s_float[]; return s_float; }
-};
-
-template <>
-struct SharedMemory <double>
-{
-    __device__ double* getPointer() { extern __shared__ double s_double[]; return s_double; }
-};
-
-template <>
-struct SharedMemory <uchar4>
-{
-    __device__ uchar4* getPointer() { extern __shared__ uchar4 s_uchar4[]; return s_uchar4; }
-};
-
-
-#endif //_SHAREDMEM_H_
+#undef SPEC_SHAREDMEM
+#endif // __SHARED_MEM_H__
 
 // Leave this at the end of the file
 // Local Variables:
