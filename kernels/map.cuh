f1c0f182691dd846c7d34e836952b94727702dbe
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2009-09-11 08:36:48 +0900

    improve the calling behaviour of these kernels, and fix thread id range checking
    Ignore-this: 45d880395060bbd5f1bb5098c2ce75c3
    
    darcs-hash:20090910233648-9241b-68102822b028549a64b2a1007b7d5c2c7e8c73aa.gz

--- kernels/map.cuh
+++ kernels/map.cuh
@@ -13,16 +13,38 @@
  * Apply a function each element of an array. A single thread is used to compute
  * each result. The input and output array may be coincident.
  */
-template <class fn, typename Ta, typename Tb>
-__global__ void
-map
+template <class fn, bool lengthIsPow2 typename Ta, typename Tb>
+__global__ static void
+map_core
 (
-    Ta *xs,
-    Tb *out
+    Ta  *xs,
+    Tb  *out,
+    int length
 )
 {
     unsigned int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    out[idx]         = fn::apply(xs[idx]);
+
+    if (lengthIsPow2 || idx < length)
+        out[idx] = fn::apply(xs[idx]);
+}
+
+
+template <class fn, typename Ta, typename Tb>
+void
+map
+(
+    Ta  *xs,
+    Tb  *out,
+    int length
+)
+{
+    unsigned int threads = min(ceilPow2(length), 512);
+    unsigned int blocks  = (length + threads - 1) / threads;
+
+    if (isPow2(length))
+        map_core< fn,true ><<<blocks,threads>>>(xs, out, length);
+    else
+        map_core< fn,false ><<<blocks,threads>>>(xs, out, length);
 }
 
 #endif
--- kernels/zipWith.cuh
+++ kernels/zipWith.cuh
@@ -13,17 +13,40 @@
  * Combine two arrays using the given binary operator function. A single thread
  * is used to compute each result pair.
  */
-template <class op, typename Ta, typename Tb, typename Tc>
-__global__ void
-zipWith
+template <class op, bool lengthIsPow2, typename Ta, typename Tb, typename Tc>
+__global__ static void
+zipWith_core
 (
-    Ta *xs,
-    Tb *ys,
-    Tc *out
+    Ta  *xs,
+    Tb  *ys,
+    Tc  *out,
+    int length
 )
 {
     unsigned int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    out[idx]         = op::apply(xs[idx], ys[idx]);
+
+    if (lengthIsPow2 || idx < length)
+        out[idx] = op::apply(xs[idx], ys[idx]);
+}
+
+
+template <class op, typename Ta, typename Tb, typename Tc>
+void
+zipWith
+(
+    Ta  *xs,
+    Tb  *ys,
+    Tc  *zs,
+    int length
+)
+{
+    unsigned int threads = min(ceilPow2(length), 512);
+    unsigned int blocks  = (length + threads - 1) / threads;
+
+    if (isPow2(length))
+        zipWith_core< op,true ><<<blocks,threads>>>(xs, ys, zs, length);
+    else
+        zipWith_core< op,false ><<<blocks,threads>>>(xs, ys, zs, length);
 }
 
 #endif
