6f86a3b71cbcc17b896c55eceed890dd2042795e
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-01-18 22:43:45 +0900

    link to smvm and sort kernels, more optimisation compile flags
    Ignore-this: 868d882aeb7776412b51de14e3433a3
    
    darcs-hash:20100118134345-9241b-db7b3f8f546724b76057565122e82c140aa72fe0.gz

--- src/common/common.mk
+++ src/common/common.mk
@@ -137,7 +137,7 @@ ifeq ($(dbg),1)
     LIBSUFFIX   := D
 else
     COMMONFLAGS += -O2
-    GHCFLAGS	+= -O2
+    GHCFLAGS	+= -O2 -fvia-C -optc-O3 -funbox-strict-fields
     BINSUBDIR   := release
     LIBSUFFIX   :=
     NVCCFLAGS   += --compiler-options -fno-strict-aliasing
