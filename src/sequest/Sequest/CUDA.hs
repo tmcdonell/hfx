579bcdb3cf8648fd96516e3a48f1e81d0d46d6c3
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-01-20 07:45:56 +0900

    sequest search, cuda version
    Ignore-this: 79c0c32a23e8cf189918e0c8eb379dd9
    - uploading theoretical spectra for all database permutations requires far too
      much memory (tens of gigabytes), so this still requires some pre-filtering
      based on the input data. Since this currently copies data from lists (FIXME),
      this is not good for benchmarking.
    
    darcs-hash:20100119224556-9241b-15b875e69f0c3be73c3e3f2a19d3b740476e9f76.gz

---
+++ src/sequest/Sequest/CUDA.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module    : Sequest.CUDA
+-- Copyright : (c) 2009 Trevor L. McDonell
+-- License   : BSD
+--
+--------------------------------------------------------------------------------
+
+module Sequest.CUDA where
+
+import Config
+import Protein
+import Spectrum
+import IonSeries
+import Sequest.Base                     (MatchCollection, Match(..), findCandidates)
+
+import Foreign
+import Foreign.C
+import Foreign.CUDA (DevicePtr, withDevicePtr)
+import qualified Foreign.CUDA           as C
+import qualified Data.Vector.Storable   as V
+
+
+--------------------------------------------------------------------------------
+-- Database search, CUDA version
+--------------------------------------------------------------------------------
+
+searchForMatches :: ConfigParams CFloat -> ProteinDatabase CFloat -> Spectrum CFloat -> IO (MatchCollection CFloat)
+searchForMatches cp db spec =
+  -- setup
+  --
+  C.withListArray offsets                   $ \d_rowPtr ->
+  C.withListArray ix                        $ \d_colIdx ->
+  C.withListArray val                       $ \d_data   ->
+  C.withListArray (V.toList specExp)        $ \d_x      ->
+  C.allocaArray num_peptides                $ \d_y      ->
+  C.withListArray [0 .. (num_peptides - 1)] $ \d_i      -> do
+
+    -- score
+    --
+    cu_smvm_f d_y d_x d_data d_rowPtr d_colIdx num_peptides
+    cu_sort_f d_y d_i num_peptides
+
+    -- retrieve results
+    --
+    r <- C.peekListArray n (d_y `C.plusDevPtr` ((num_peptides-n) * sizeOf (undefined::CFloat)))
+    i <- C.peekListArray n (d_i `C.plusDevPtr` ((num_peptides-n) * sizeOf (undefined::CFloat)))
+    return $ reverse (zipWith k i r)
+  where
+    n            = max (numMatches cp) (numMatchesDetail cp)
+    k i r        = Match (concatMap fragments candidates !! i) (r/10000)
+    bnds         = (0, fromIntegral (V.length specExp -1))
+    offsets      = scanl (+) 0 . map (fromIntegral . length) $ specThry
+    (ix,val)     = unzip . concat $ specThry
+    num_peptides = length offsets - 1
+
+    candidates   = findCandidates cp spec . map (digestProtein cp) $ db
+    specExp      = buildExpSpecXCorr cp spec
+    specThry     = [ buildThrySpecXCorr cp (charge spec) bnds peptide
+                      | protein <- candidates
+                      , peptide <- fragments protein ]
+
+
+--------------------------------------------------------------------------------
+-- FFI Bindings
+--------------------------------------------------------------------------------
+
+cu_sort_f :: DevicePtr CFloat -> DevicePtr a -> Int -> IO ()
+cu_sort_f d_keys d_vals l =
+  withDevicePtr d_keys $ \keys ->
+  withDevicePtr d_vals $ \vals -> do
+    cu_sort_f' keys (castPtr vals) (fromIntegral l)
+
+
+cu_smvm_f :: DevicePtr CFloat -> DevicePtr CFloat -> DevicePtr CFloat -> DevicePtr CUInt -> DevicePtr CUInt -> Int -> IO ()
+cu_smvm_f d_y d_x d_vals d_ptr d_idx num_rows =
+  withDevicePtr d_y    $ \p_y    ->
+  withDevicePtr d_x    $ \p_x    ->
+  withDevicePtr d_vals $ \p_vals ->
+  withDevicePtr d_ptr  $ \p_ptr  ->
+  withDevicePtr d_idx  $ \p_idx  -> do
+    cu_smvm_f' p_y p_x p_vals p_ptr p_idx (fromIntegral num_rows)
+
+
+foreign import ccall unsafe "algorithms.h sort_f" cu_sort_f'
+  :: Ptr CFloat -> Ptr () -> CUInt -> IO ()
+
+foreign import ccall unsafe "algorithms.h smvm_f" cu_smvm_f'
+  :: Ptr CFloat -> Ptr CFloat -> Ptr CFloat -> Ptr CUInt -> Ptr CUInt -> CUInt -> IO ()
+
