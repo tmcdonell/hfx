28c79a51fff46650b2e46c428d48c785add76827
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-02-26 15:39:25 +0900

    split some overly long function declarations
    Ignore-this: 303ea1f5cbe0d563ab03412689dea6f3
    
    darcs-hash:20100226063925-9241b-d9821c04d41544605d6a8760bfc3634f7cf2d65e.gz

--- src/common/include/algorithms.h
+++ src/common/include/algorithms.h
@@ -10,6 +10,8 @@
 #ifndef __ALGORITHMS_H__
 #define __ALGORITHMS_H__
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -21,37 +23,65 @@ extern "C" {
  *   Pre-scans are exclusive:   prescanl  == init . scanl
  *   Post-scans are inclusive:  postscanl == scanl1
  */
-void prescanl_plusui(const unsigned int *d_in, unsigned int *d_out, const unsigned int N);
-void prescanr_plusui(const unsigned int *d_in, unsigned int *d_out, const unsigned int N);
+void prescanl_plusui(const uint32_t *d_in, uint32_t *d_out, const uint32_t N);
+void prescanr_plusui(const uint32_t *d_in, uint32_t *d_out, const uint32_t N);
 
 /*
  * Radix sort (in-place)
  */
-void radixsort_f(float *d_keys, void *d_vals, unsigned int N);
+void radixsort_f(float *d_keys, void *d_vals, uint32_t N);
 
 /*
  * Permute (32-bit payload)
  */
-void permute(const void *d_in, void *d_out, const unsigned int *d_indices, const unsigned int length);
-void bpermute(const void *d_in, void *d_out, const unsigned int *d_indices, const unsigned int length);
-unsigned int compact(const void *d_in, void *d_out, const unsigned int *d_flags, const unsigned int length);
-unsigned int compactIndices(unsigned int *d_out, const unsigned int *d_flags, const unsigned int length);
+void permute(const void *d_in, void *d_out, const uint32_t *d_indices, const uint32_t length);
+void bpermute(const void *d_in, void *d_out, const uint32_t *d_indices, const uint32_t length);
+uint32_t compact(const void *d_in, void *d_out, const uint32_t *d_flags, const uint32_t length);
+uint32_t compactIndices(uint32_t *d_out, const uint32_t *d_flags, const uint32_t length);
 
 /*
  * Filter
  */
-unsigned int filterInRange_f(const float *d_in, float *d_out, const unsigned int length, const float min, const float max);
-unsigned int findIndicesInRange_f(const float *d_in, unsigned int *d_out, const unsigned int length, const float min, const float max);
+uint32_t
+filterInRange_f
+(
+    const float         *d_in,
+    float               *d_out,
+    const uint32_t      length,
+    const float         min,
+    const float         max
+);
+
+uint32_t
+findIndicesInRange_f
+(
+    const float         *d_in,
+    uint32_t            *d_out,
+    const uint32_t      length,
+    const float         min,
+    const float         max
+);
 
 /*
  * Generate theoretical spectra
  */
-void addIons(unsigned int *d_spec, const float *d_residuals, const float *d_ladder, const unsigned int *d_rowPtr, const unsigned int *d_inRangeIdx, const unsigned int num_inRange, const unsigned int max_charge, const unsigned int len_spec);
+void
+addIons
+(
+    uint32_t            *d_spec,
+    const float         *d_residual,
+    const float         *d_ladder,
+    const uint32_t      *d_rowPtr,
+    const uint32_t      *d_inRangeIdx,
+    const uint32_t      num_inRange,
+    const uint32_t      max_charge,
+    const uint32_t      len_spec
+);
 
 /*
  * Dense matrix-vector multiplication
  */
-void mvm_if(float *d_y, const unsigned int *d_A, const float *d_x, const unsigned int m, const unsigned int n);
+void mvm_if(float *d_y, const uint32_t *d_A, const float *d_x, const uint32_t m, const uint32_t n);
 
 
 #ifdef __cplusplus
