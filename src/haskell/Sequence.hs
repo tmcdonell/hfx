a9a3411a5a75d13b1729a8f770e8df26904a3080
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-03-06 18:03:11 +0900

    Reading and processing sequences into flattened vectors
    Ignore-this: 599da53e669ebce33f065c8240c4d9d2
    - This is the result of some experimentation, and should be able to generate
      large vectors fairly efficiently.
    - Unboxed vectors are used for their flexibility, and seemingly higher
      performance over the Storable variety (at least in the case of ionMasses).
    
    darcs-hash:20100306090311-9241b-8b1986479371b23e6f2078cdb441198cfa78455d.gz

---
+++ src/haskell/Sequence.hs
@@ -0,0 +1,151 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module    : Sequence
+-- Copyright : (c) 2010 Trevor L. McDonell
+-- License   : BSD
+--
+-- Reading and processing sequence related stuff
+--
+--------------------------------------------------------------------------------
+
+{-# LANGUAGE BangPatterns  #-}
+{-# LANGUAGE TupleSections #-}
+
+module Sequence
+  (
+    readFasta, countSeqs,
+    ionMasses,
+    peptides,
+  )
+  where
+
+import Mass
+import Config
+
+import Data.Int
+import Data.List
+import Control.Applicative
+
+import qualified Bio.Sequence               as F
+import qualified Bio.Sequence.Fasta         as F
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Codec.Compression.GZip     as GZip
+
+import qualified Data.Vector.Unboxed        as U
+
+
+--------------------------------------------------------------------------------
+-- Reading FASTA Files
+--------------------------------------------------------------------------------
+
+--
+-- Lazily read sequences from a FASTA-formatted file. This is identical to the
+-- code of the bio package, except the sequence type is cast on output and GZip
+-- compressed files are deflated as necessary.
+--
+readFasta :: FilePath -> IO [F.Sequence F.Amino]
+readFasta fp = map F.castToAmino . F.mkSeqs . L.lines . prepare <$> L.readFile fp
+  where
+    prepare = if ".gz" `isSuffixOf` fp then GZip.decompress
+                                       else id
+
+--
+-- Count the number of sequences in a file. Each sequence consist of a header
+-- and a set of lines containing the sequence data. GZip compressed files are
+-- inflated if necessary, which is the only addition over the bio package
+-- function of the same name.
+--
+countSeqs :: FilePath -> IO Int
+countSeqs fp = length . headers . prepare <$> L.readFile fp
+  where
+    headers = filter (('>' ==) . L.head) . filter (not . L.null) . L.lines
+    prepare = if ".gz" `isSuffixOf` fp then GZip.decompress
+                                       else id
+
+
+--------------------------------------------------------------------------------
+-- Digestion
+--------------------------------------------------------------------------------
+
+fst3 :: (a,b,c) -> a
+fst3 (a,_,_) = a
+
+snd3 :: (a,b,c) -> b
+snd3 (_,b,_) = b
+
+thd3 :: (a,b,c) -> c
+thd3 (_,_,c) = c
+
+
+--
+-- Translate the amino acid character codes of the database into the
+-- corresponding ion masses in a single, flattened vector.
+--
+ionMasses :: ConfigParams -> [F.Sequence F.Amino] -> U.Vector Float
+ionMasses cp = U.unfoldr step . map F.seqdata
+  where
+    step []     = Nothing
+    step (s:ss) | L.null s  = step ss
+                | otherwise = Just (getAAMass cp (L.head s), L.tail s : ss)
+
+
+--
+-- Scan the input sequences to generate a flattened vector of the peptide
+-- fragments found using the current digestion rules. The output indices are
+-- global offsets, not into the individual parent sequence. This is suitable for
+-- indexing into the similarly globally concatenated 'ionMass' output.
+--
+peptides :: ConfigParams -> [F.Sequence F.Amino] -> U.Vector (Float,Int32,Int32)
+peptides cp db
+  = U.fromList . concat
+  . zipWith offset (scanl (+) 0 . map (fromIntegral . L.length . F.seqdata) $ db)
+  . map digest
+  $ db
+  where
+    digest    = filter (inrange . fst3) . splice cp . fragment cp
+    offset o  = map (\(r,c,n) -> (r,c+o,n+o))
+
+    --
+    -- The mass of the peptide is the sum of the amino acid residue masses plus
+    -- the mass of the water molecule released in forming the peptide bond (plus
+    -- one; from Eq. 1 of Eng.[1])
+    --
+    inrange m = let m' = m + massH2O + massH
+                in  minPeptideMass cp <= m' && m' <= maxPeptideMass cp
+
+
+--
+-- Split a protein sequence with the given digestion rule. This outputs the
+-- total residual mass of the fragment, together with the indices of the C- and
+-- N- terminals from parent sequence.
+--
+fragment :: ConfigParams -> F.Sequence F.Amino -> [(Float,Int32,Int32)]
+fragment cp = unfoldr step . (0,) . F.seqdata
+  where
+    rule = fst . digestionRule $ cp
+
+    -- add strictness here?
+    --
+    chunk c n s = let (h,t) = L.splitAt (n+1) s
+                      mass  = L.foldl' (\m a -> m + getAAMass cp a) 0 h
+                  in  Just ((mass, fromIntegral c, fromIntegral (c+n)), (c+n+1, t))
+
+    step (!o,!s) = case L.findIndex rule s of
+      Nothing -> if L.null s then Nothing
+                             else chunk o (L.length s - 1) s
+      Just !i -> chunk o i s
+
+
+--
+-- Generate additional sequences from missed cleavages of sequential, adjacent
+-- peptide fragments.
+--
+splice :: ConfigParams -> [(Float,Int32,Int32)] -> [(Float,Int32,Int32)]
+splice cp = loop
+  where
+    loop []     = []
+    loop (p:ps) = scanl join p (take n ps) ++ loop ps
+
+    n        = missedCleavages cp
+    join a b = (fst3 a + fst3 b, snd3 a, thd3 b)
+
