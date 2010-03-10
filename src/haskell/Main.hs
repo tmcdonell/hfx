98eb7f734c36ad94a3f5e3f2b3a67aa924483c8d
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-03-10 11:48:58 +0900

    CUDA-based protein matching, with pretty printing.
    Ignore-this: 6cee168b187f8d50d58d06324fc7e56b
    
    darcs-hash:20100310024858-9241b-bb31f0aae73089c8609d9c1f2b4fde8de57a5ca0.gz

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -16,18 +16,15 @@ module Main where
 import DTA
 import Utils
 import Config
-import Protein
+import Sequest
+import Sequence
 import PrettyPrint
-
-import Database
-import qualified CUDA.PDB       as PDB
---import qualified CUDA.SMVM      as SMVM
 
 --
 -- System libraries
 --
-import Control.Monad                                    (when)
-import System.Environment                               (getArgs)
+import Control.Monad            (when)
+import System.Environment       (getArgs)
 import System.IO
 import qualified Foreign.CUDA   as CUDA
 
@@ -47,27 +44,25 @@ defaultConfigFile =  "sequest.params"
 main :: IO ()
 main = do
   (cp,files) <- sequestConfig defaultConfigFile =<< getArgs
-  proteins   <- maybe (error "Protein database not specified") (digestFasta cp) (databasePath cp)
+  sequences  <- maybe (error "Protein database not specified") readFasta (databasePath cp)
 
   when (verbose cp && not (useCPU cp)) $ do
     dev   <- CUDA.get
     props <- CUDA.props dev
-    hPutStrLn stderr $ "> device " ++ shows dev ": " ++ CUDA.deviceName props
+    hPutStrLn stderr $ "> device " ++ shows dev ": "
+                                   ++ CUDA.deviceName props
+                                   ++ " (compute " ++ shows (CUDA.computeCapability props) ")"
 
-  withPDB cp proteins $ \pdb -> mapM_ (search cp pdb) files
+  withSeqs cp sequences $ \pdb -> mapM_ (search cp pdb) files
 
 
 --
 -- Search the protein database for a match to the experimental spectra
 --
-search :: ConfigParams Float -> ProteinDatabase Float -> FilePath -> IO ()
+search :: ConfigParams -> ProteinDatabase -> FilePath -> IO ()
 search cp pdb fp = do
   dta     <- forceEitherStr `fmap` readDTA fp
-  matches <- PDB.searchForMatches cp pdb dta
-
---  when (verbose cp) $
---    let ref = searchForMatches cp proteins dta in
---    hPutStrLn stderr $ "> verify: " ++ shows (zipWith (==) ref matches) "\n"
+  matches <- searchForMatches cp pdb dta
 
   printConfig cp fp dta
   printResults       $! (take (numMatches cp)       matches)
