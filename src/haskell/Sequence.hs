0fabfe9ce64d7321ee5ab2382ea4498e8556c8f9
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-03-18 12:44:04 +0900

    Optimisations for reading a sequence file
    Ignore-this: 8417fc0eaf14bca4e6ab9a4009fa779f
    - In testing, this method generates data at an average of ~20MB/s, requiring
      about 40s to read the "uniprot_sprot.fasta" database (Logos).
    - There are sure to be optimisations about. For example, lists are still in use.
    
    darcs-hash:20100318034404-9241b-bd90d47a1ad016f0adb790ec845bdc7fdd5e3d37.gz

--- src/haskell/Sequence.hs
+++ src/haskell/Sequence.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE BangPatterns  #-}
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE CPP          #-}
+{-# LANGUAGE BangPatterns #-}
 --------------------------------------------------------------------------------
 -- |
 -- Module    : Sequence
@@ -13,37 +12,76 @@
 
 module Sequence
   (
-    Protein,
-    readFasta, countSeqs, countIons,
-    ionMasses, peptides, digest,
-
-    Fragment(..), fraglabel
+    SequenceDB(..), Fragment(..), SKey,
+    makeSeqDB, lookup, fraglabel
   )
   where
 
 import Mass
 import Config
 
+import Prelude                                  hiding (lookup)
+import Data.Int
+import Data.Char
+import Data.List                        (unfoldr, isSuffixOf)
 import Data.Word
-import Data.List
-import Control.Applicative
+import Control.Monad                    (foldM)
+import Control.Applicative              ((<$>))
 
-import qualified Bio.Sequence                      as F
-import qualified Bio.Sequence.Fasta                as F
-import qualified Data.ByteString.Lazy.Char8        as L
-import qualified Codec.Compression.GZip            as GZip
+import qualified Bio.Sequence                   as F
+import qualified Bio.Sequence.Fasta             as F
+import qualified Data.ByteString.Lazy           as L
+import qualified Data.ByteString.Lazy.Char8     as LC
+import qualified Codec.Compression.GZip         as GZip
 
-import qualified Data.Vector.Unboxed               as U
-import qualified Data.Vector.Generic               as G
-import qualified Data.Vector.Fusion.Stream.Monadic as S
-import qualified Data.Vector.Fusion.Stream.Size    as S
-import Data.Vector.Fusion.Util
+import qualified Data.Vector                    as V
+import qualified Data.Vector.Unboxed            as U
+import qualified Data.Vector.Generic            as G
+import qualified Data.Vector.Generic.Mutable    as GM
+import qualified Data.Vector.Fusion.Stream      as S
+import qualified Data.Vector.Fusion.Stream.Size as S
+
+#if defined(__GLASGOW_HASKELL__)
+import GHC.Base                         (unsafeChr)
+#endif
 
 #define PHASE_STREAM [1]
 #define PHASE_INNER  [0]
 
 #define INLINE_STREAM INLINE PHASE_STREAM
 #define INLINE_INNER  INLINE PHASE_INNER
+
+
+--
+-- Extract components from a three-tuple
+--
+fst3 :: (a,b,c) -> a
+fst3 (a,_,_) = a
+{-# INLINE fst3 #-}
+
+snd3 :: (a,b,c) -> b
+snd3 (_,b,_) = b
+{-# INLINE snd3 #-}
+
+thd3 :: (a,b,c) -> c
+thd3 (_,_,c) = c
+{-# INLINE thd3 #-}
+
+--
+-- Conversion between 'Word8' and 'Char'. Should compile to a no-op.
+--
+w2c :: Word8 -> Char
+#if !defined(__GLASGOW_HASKELL__)
+w2c = chr . fromIntegral
+#else
+w2c = unsafeChr . fromIntegral
+#endif
+{-# INLINE w2c #-}
+
+c2w :: Char -> Word8
+c2w = fromIntegral . ord
+{-# INLINE c2w #-}
+
 
 
 --------------------------------------------------------------------------------
@@ -59,7 +97,7 @@ type Protein = F.Sequence F.Amino
 --
 readFasta :: FilePath -> IO [Protein]
 {-# INLINE readFasta #-}
-readFasta fp = map F.castToAmino . F.mkSeqs . L.lines . prepare <$> L.readFile fp
+readFasta fp = map F.castToAmino . F.mkSeqs . LC.lines . prepare <$> L.readFile fp
   where
     prepare = if ".gz" `isSuffixOf` fp then GZip.decompress
                                        else id
@@ -74,9 +112,79 @@ countSeqs :: FilePath -> IO Int
 {-# INLINE countSeqs #-}
 countSeqs fp = length . headers . prepare <$> L.readFile fp
   where
-    headers = filter (('>' ==) . L.head) . filter (not . L.null) . L.lines
+    headers = filter (('>' ==) . LC.head) . filter (not . L.null) . LC.lines
     prepare = if ".gz" `isSuffixOf` fp then GZip.decompress
                                        else id
+
+
+--------------------------------------------------------------------------------
+-- Sequences
+--------------------------------------------------------------------------------
+
+type SKey = Int32
+
+--
+-- A collection of protein sequences
+--
+data SequenceDB = SeqDB
+  {
+    dbHeader  :: V.Vector L.ByteString,          -- sequence ion description headers
+    dbIon     :: U.Vector Word8,                 -- flattened array of amino character codes
+    dbIonSeg  :: U.Vector Int32,                 -- segmenting information for ions
+    dbFrag    :: U.Vector (Float, Int32, Int32), -- (residual mass, c-idx, n-idx)
+    dbFragSeg :: U.Vector Int32                  -- fragment segmenting information for deriving parent
+  }
+  deriving Show
+
+--
+-- Locate a particular sequence in the database
+--
+lookup :: SKey -> SequenceDB -> Maybe Fragment
+lookup =  undefined
+
+
+--
+-- Generate a sequence database from the given file. This is a flattened
+-- representation of ion character codes and fragment information, together with
+-- segmenting information describing the boundaries of the original Proteins
+-- they derive from.
+--
+makeSeqDB :: ConfigParams -> FilePath -> IO SequenceDB
+{-# INLINE makeSeqDB #-}
+makeSeqDB cp fp = do
+  ns <- countSeqs fp
+  db <- readFasta fp
+
+  let iseg = ionSeg  ns db
+      hdr  = headers ns db
+      ions = ionSeq (fromIntegral (U.last iseg)) db
+      nf   = countFrags cp ions
+
+  f  <- GM.new nf               -- maximum number as estimated above, trim later
+  fs <- GM.new (ns+1)           -- include the zero index
+
+  --
+  -- OPT: make versions that operate directly from (boxed) streams?
+  --
+  let put !i !v = do GM.unsafeWrite f i v
+                     return (i+1)
+
+  let fill (!i,!n) (!x,!y) = do GM.unsafeWrite fs i (fromIntegral n)
+                                n' <- foldM put n (digest cp ions (x,y))
+                                return (i+1, n')
+
+  --
+  -- Now iterate over all of the protein sequences, keeping track of the segment
+  -- number and number of fragments generated so far, so we can also fill in the
+  -- fragment segmenting information.
+  --
+  nf' <- snd <$> foldM fill (0,0) (G.toList (G.zip iseg (G.tail iseg)))
+  GM.unsafeWrite fs ns (fromIntegral nf')
+
+  f'  <- G.unsafeFreeze (GM.take nf' f)
+  fs' <- G.unsafeFreeze fs
+
+  return $ SeqDB hdr ions iseg f' fs'
 
 
 --------------------------------------------------------------------------------
@@ -84,41 +192,26 @@ countSeqs fp = length . headers . prepare <$> L.readFile fp
 --------------------------------------------------------------------------------
 
 --
--- Sequence fragments (typically to store results for pretty printing)
+-- Sequence fragments
 --
-data Fragment = Fragment
+data Fragment = Frag
   {
-    fragmass   :: Float,
-    fragheader :: L.ByteString,
-    fragdata   :: L.ByteString
+    fragmass   :: Float,                -- Total mass of this fragment
+    fragheader :: L.ByteString,         -- Full header describing this fragment
+    fragdata   :: L.ByteString          -- Fragment sequence data, including flanking residuals
   }
   deriving (Eq, Show)
 
+--
+-- Fragment label (first word of full header)
+--
 fraglabel :: Fragment -> L.ByteString
-fraglabel = head . L.words . fragheader
+fraglabel = head . LC.words . fragheader
 
 
 --------------------------------------------------------------------------------
 -- Digestion
 --------------------------------------------------------------------------------
-
-fst3 :: (a,b,c) -> a
-fst3 (a,_,_) = a
-
-snd3 :: (a,b,c) -> b
-snd3 (_,b,_) = b
-
-thd3 :: (a,b,c) -> c
-thd3 (_,_,c) = c
-
-
---
--- Determine the total number of ions represented by the protein sequences
---
-countIons :: [Protein] -> Int
-{-# INLINE countIons #-}
-countIons = fromIntegral . foldl (+) 0 . map (L.length . F.seqdata)
-
 
 --
 -- Estimate the number of fragments that will be produced from a collection of
@@ -129,63 +222,40 @@ countIons = fromIntegral . foldl (+) 0 . map (L.length . F.seqdata)
 -- The latter accounts for a rather small discrepancy (<1%), while the former
 -- may result in significant overestimation (>10%).
 --
-countFrags :: ConfigParams -> [Protein] -> Int
+countFrags :: ConfigParams -> U.Vector Word8 -> Int
 {-# INLINE countFrags #-}
-countFrags cp = foldl' (+) 0 . map (count . F.seqdata)
+countFrags cp = ((missedCleavages cp + 1) * ) . count
   where
-    rule  = fst (digestionRule cp)
-    count = ((missedCleavages cp + 1) * ) . length . L.findIndices rule
+    rule  = fst (digestionRule cp) . w2c
+    count = U.length . U.filter rule
 
 
 --
--- Translate the amino acid character codes of the database into the
--- corresponding ion masses in a single, flattened vector.
+-- Extract sequence headers
 --
-ionMasses :: ConfigParams -> Int -> [Protein] -> U.Vector Float
-{-# INLINE ionMasses #-}
-ionMasses cp n db = G.unstream ionS
-  where
-    {-# INLINE_STREAM ionS #-}
-    ionS = S.Stream (return . step) (map F.seqdata db) (S.Exact (delay_inline max n 0))
-
-    {-# INLINE_INNER step #-}
-    step []     = S.Done
-    step (s:ss) = case L.uncons s of
-                    Nothing    -> S.Skip ss
-                    Just (c,t) -> S.Yield (getAAMass cp c) (t:ss)
+headers :: Int -> [Protein] -> V.Vector L.ByteString
+{-# INLINE headers #-}
+headers n db
+  = G.unstream
+  $ S.fromList (map F.seqheader db) `S.sized` S.Exact n
 
 
 --
--- Global indices for the start of each sequence fragment block in, for example,
--- the vector generated by `ionMasses`.
+-- Extract the amino acid character codes and segmenting information from the
+-- given list of proteins
 --
-seqOffset :: Int -> [Protein] -> U.Vector Int
-{-# INLINE seqOffset #-}
-seqOffset n = G.unstream . offsetS
-  where
-    {-# INLINE_STREAM offsetS #-}
-    offsetS db = S.prescanl' (+) 0
-               $ S.Stream (return . step) (map F.seqdata db) (S.Exact (delay_inline max n 0))
-
-    {-# INLINE_INNER step #-}
-    step []     = S.Done
-    step (s:ss) = S.Yield (fromIntegral (L.length s)) ss
+ionSeq :: Int -> [Protein] -> U.Vector Word8
+{-# INLINE ionSeq #-}
+ionSeq n db
+  = G.unstream
+  $ S.fromList (concatMap (L.unpack . F.seqdata) db) `S.sized` S.Exact n
 
 
---
--- Scan the input sequences to generate a flattened vector of the peptide
--- fragments found using the current digestion rules. The output indices are
--- global offsets, not into the individual parent sequence. This is suitable for
--- indexing into the similarly globally concatenated 'ionMass' output.
---
-peptides :: ConfigParams -> [Protein] -> U.Vector (Float,Word32,Word32)
-peptides cp db
-  = U.fromList . concat
-  . zipWith global offset
-  $ map (digest cp) db
-  where
-    offset   = scanl (+) 0 . map (fromIntegral . L.length . F.seqdata) $ db
-    global o = map (\(r,c,n) -> (r,c+o,n+o))
+ionSeg :: Int -> [Protein] -> U.Vector Int32
+{-# INLINE ionSeg #-}
+ionSeg n db
+  = G.unstream
+  $ S.fromList (scanl (+) 0 $ map (fromIntegral . L.length . F.seqdata) db) `S.sized` S.Exact (n+1)
 
 
 --
@@ -196,40 +266,42 @@ peptides cp db
 -- mass of the water molecule released in forming the peptide bond (plus one;
 -- from Eq. 1 of Eng.[1])
 --
-digest :: ConfigParams -> Protein -> [(Float,Word32,Word32)]
-digest cp = filter (inrange . fst3) . splice cp . fragment cp
+digest :: ConfigParams -> U.Vector Word8 -> (Int32,Int32) -> [(Float,Int32,Int32)]
+{-# INLINE digest #-}
+digest cp ions (!x,!y) = filter (inrange . fst3) . splice cp . fragment cp x $ segment
   where
+    segment   = G.unsafeSlice (fromIntegral x) (fromIntegral (y-x)) ions
     inrange m = let m' = m + massH2O + massH
                 in  minPeptideMass cp <= m' && m' <= maxPeptideMass cp
-
 
 --
 -- Split a protein sequence with the given digestion rule. This outputs the
 -- total residual mass of the fragment, together with the indices of the C- and
--- N- terminals from parent sequence.
+-- N- terminals in global index coordinates.
 --
-fragment :: ConfigParams -> Protein -> [(Float,Word32,Word32)]
-fragment cp = unfoldr step . (0,) . F.seqdata
+fragment :: ConfigParams -> Int32 -> U.Vector Word8 -> [(Float,Int32,Int32)]
+{-# INLINE fragment #-}
+fragment cp gid ions = unfoldr step (gid,ions)
   where
-    rule = fst . digestionRule $ cp
+    rule = fst (digestionRule cp) . w2c
+    mass = G.foldl' (+) 0 . G.map (getAAMass cp . w2c)
 
-    -- add strictness here?
-    --
-    chunk c n s = let (h,t) = L.splitAt (n+1) s
-                      mass  = L.foldl' (\m a -> m + getAAMass cp a) 0 h
-                  in  Just ((mass, fromIntegral c, fromIntegral (c+n)), (c+n+1, t))
-
-    step (!o,!s) = case L.findIndex rule s of
-      Nothing -> if L.null s then Nothing
-                             else chunk o (L.length s - 1) s
-      Just !i -> chunk o i s
+    step (!c,!v)
+      | G.null v  = Nothing
+      | otherwise = case G.findIndex rule v of
+                      Nothing -> Just ((mass v, c, c + fromIntegral (G.length v)), (0, G.empty))
+                      Just !i -> let n = fromIntegral i
+                                     h = U.take (i+1) v
+                                     t = U.drop (i+1) v
+                                 in Just ((mass h, c, c+n), (c+n+1,t))
 
 
 --
 -- Generate additional sequences from missed cleavages of sequential, adjacent
 -- peptide fragments.
 --
-splice :: ConfigParams -> [(Float,Word32,Word32)] -> [(Float,Word32,Word32)]
+splice :: ConfigParams -> [(Float,Int32,Int32)] -> [(Float,Int32,Int32)]
+{-# INLINE splice #-}
 splice cp = loop
   where
     loop []     = []
