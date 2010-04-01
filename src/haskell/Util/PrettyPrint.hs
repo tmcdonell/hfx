06ec52eff2dc67b4e0dffb432f1b05c82b75a076
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-04-01 13:24:27 +0900

    display info/title field in results
    Ignore-this: 74886d2fa3f73a7cfedd1d87706ff2a1
    - currently only extracted from MGF files
    
    darcs-hash:20100401042427-9241b-1abdd1c915b80d64118eed468319f28919dd168d.gz

--- src/haskell/Util/PrettyPrint.hs
+++ src/haskell/Util/PrettyPrint.hs
@@ -71,7 +71,8 @@ printDoc m hdl doc = do
 
 printConfig :: ConfigParams -> FilePath -> MS2Data -> IO ()
 printConfig cp fp ms2 = displayIO . ppAsRows 0 . map (intersperse (text "::")) $
-    [ [text "Spectrum"   , text fp]
+    [ [text "Title"      , ppr (ms2info ms2)]
+    , [text "Spectrum"   , text fp]
     , [text "Database"   , text (fromJust (databasePath cp))]
     , [text "Enzyme"     , hsep $ map text (tail enzyme)]
     , [text "(M+H)+ Mass", float mass <+> text "~" <+> float (realToFrac $ massTolerance cp)]
