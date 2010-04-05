b5c3da6192a31edcc48e9d8a39e5f1d4ebaf31a7
Trevor McDonell <tmcdonell@cse.unsw.edu.au>
2010-04-05 21:36:50 +0900

    utility program to digest a sequence file and save for later use
    Ignore-this: d404cea62f1006e7beb6bd9e0162a62f
    
    darcs-hash:20100405123650-9241b-6a4c498adf761a1fc14f95854abb9fe2ff7ae302.gz

---
+++ src/haskell/MainGen.hs
@@ -0,0 +1,38 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module    : MainGen
+-- Copyright : (c) [2009..2010] Trevor L. McDonell
+-- License   : BSD
+--
+-- Generate a list of peptides that meet the digestion criteria
+--
+--------------------------------------------------------------------------------
+
+module Main where
+
+import Config
+import Sequence.Index
+import Sequence.Fragment
+
+import Data.Maybe
+import System.IO
+import System.Environment
+import qualified Data.Vector.Generic as G
+
+
+main :: IO ()
+main = do
+  args   <- getArgs
+  (cp,f) <- sequestConfig "sequest.params" args
+  let fp =  fromMaybe (error "Protein database not specified") (databasePath cp)
+
+  db <- makeSeqDB cp fp
+  if null f
+     then writeIndex stdout cp db
+     else withFile (head f) WriteMode (\h -> writeIndex h cp db)
+
+  hPutStrLn stderr $ "Database: " ++ fp
+  hPutStrLn stderr $ " # amino acids: " ++ (show . G.length . dbIon    $ db)
+  hPutStrLn stderr $ " # proteins:    " ++ (show . G.length . dbHeader $ db)
+  hPutStrLn stderr $ " # peptides:    " ++ (show . G.length . dbFrag   $ db)
+
